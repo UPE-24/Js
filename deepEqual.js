2e165ed816318027cf75f68300d631996e8df956
Mikhail R. <anarchy16@inbox.ru>
2025-06-17 09:18:21 +0300

    Update deepEqual.js
    added comments

--- deepEqual.js
+++ deepEqual.js
@@ -1,4 +1,5 @@
-var objA = {
+// используй const, let - var уже никто не использует
+const objA = {
   prop1: "value1",
   prop2: "value2",
   prop3: "value3",
@@ -12,7 +13,7 @@ var objA = {
   prop5: 1000,
   prop6: new Date(2016, 2, 10),
 };
-var objB = {
+const objB = {
   prop5: 6,
   prop3: "value3",
   prop1: "value1",
@@ -31,6 +32,15 @@ const deepEqual = (obj1, obj2) => {
   // Если obj1 === obj2 один и тот же объект товрент true (тип проверкв что оба оьекта ссылаются на одно итоже)
   if (obj1 === obj2) return true;
 
+  if (
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object" ||
+    obj1 === null || // можно сократить до !obj1 || !obj2
+    obj2 === null
+  ) {
+    return false;
+  }
+
   // Проверка дат (Date) - спиздил из инета но она работает так что збс
   if (obj1 instanceof Date && obj2 instanceof Date) {
     return obj1.getTime() === obj2.getTime();
@@ -40,24 +50,20 @@ const deepEqual = (obj1, obj2) => {
   if (Array.isArray(obj1) && Array.isArray(obj2)) {
     //Сравниваем что оба массива равны
     if (obj1.length !== obj2.length) return false;
+
     // Перебираем масив и вьбываем рекурсив до талово
     for (let i = 0; i < obj1.length; i++) {
-      if (!deepEqual(obj1[i], obj2[i])) return false;
+      if (!deepEqual(obj1[i], obj2[i])) return false; // это не очень понятно для чего
     }
+    
+    // Сравнение поэлементно
+    for (let i = 0; i < obj1.length; i++) {
+      if (obj1[i] !== obj2[i]) return false;
+    }
+
     return true;
   }
-  // Исправления баги !!!
-  //Крч тут проверка что оба — объекты и не null, тип у меня вся сверка была на то что,
-  //  я сравниваю обьеты а когда у меня попадались не обьеукты, мой код дальше думал что эт обьеты
-  if (
-    // тут у меня тоже была бага ибо оказываеттся null нужно отдельно проверять
-    typeof obj1 !== "object" ||
-    typeof obj2 !== "object" ||
-    obj1 === null ||
-    obj2 === null
-  ) {
-    return false;
-  }
+
 
   // В начле нужно проврить ключи (Object.keys) очень удобно
   // Получаем ключи объектов в массиве
@@ -68,10 +74,19 @@ const deepEqual = (obj1, obj2) => {
   for (let key of keys1) {
     // Если во втором объекте нет такого ключа — объекты разные
     if (!keys2.includes(key)) return false;
+
     // Рекурсивно сравниваем значения по ключу
     if (!deepEqual(obj1[key], obj2[key])) return false;
   }
-  // Все ключи и значения совпадают
-  return true;
+
+  /**
+   * Проверку выше можно упростить: сравниваем каждый ключ, и,
+   * если они все нашлись во втором массиве ключей и если значения этих ключей тоже совпали
+   * то значит проверка пройдена.
+   */
+  return keys1.every((key) => {
+    return keys2.includes(key) && deepEqual(obj1[key], obj2[key]);
+  })
 };
+
 console.log(deepEqual(objA, objB));
